728970d52824047736b7fd6170547205cf6aa702
Gunjan Paul <46948185+gunjanmimo@users.noreply.github.com>
2020-09-10 12:10:05 +0530

    new

--- secondAngularAPP/src/app/app.module.ts
+++ secondAngularAPP/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ServerComponent } from './server/server.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -5,7 +6,8 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule
