2b0896802b4710db192f18424da03efa5efe802e
Gunjan Paul <46948185+gunjanmimo@users.noreply.github.com>
2020-09-11 00:29:25 +0530

    new

---
+++ Project/ContactBook/src/app/all-contact/all-contact.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-all-contact',
+  templateUrl: './all-contact.component.html',
+  styleUrls: ['./all-contact.component.css']
+})
+export class AllContactComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- Project/ContactBook/src/app/app.module.ts
+++ Project/ContactBook/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AllContactComponent } from './all-contact/all-contact.component';
+import { AddContactComponent } from './add-contact/add-contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllContactComponent,
+    AddContactComponent
   ],
   imports: [
     BrowserModule
