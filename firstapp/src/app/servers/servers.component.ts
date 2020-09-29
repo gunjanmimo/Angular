d28b475e274347f1974c036cd39d3fccb0b36ab7
gunjanmimo <gunjan.mtbpaul@gmail.com>
2020-09-29 23:19:39 +0530

    new apl setup

---
+++ firstapp/src/app/servers/servers.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowServer = false;
+  serverCreationStatus = "no server is created"
+  serverName = ""
+  serverCreated = false
+  servers = ['test server', 'test server 2', 'test server 3']
+
+
+  //constructor
+  constructor() {
+    setTimeout(() => {
+      this.allowServer = true
+
+    }, 2000);
+  }
+
+  ngOnInit(): void {
+  }
+
+  onCreateServer() {
+    this.serverCreated = true
+    this.servers.push(this.serverName)
+    this.serverCreationStatus = this.serverName + ",server is created"
+  }
+  onUpdateServerName(event: any) {
+    console.log(event)
+    this.serverName = (<HTMLInputElement>event.target).value;
+
+  }
+}
