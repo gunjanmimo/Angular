0c631360a34985c850c15067ea3bbf385e59f94f
gunjanmimo <gunjan.mtbpaul@gmail.com>
2020-09-30 02:52:23 +0530

    new apl setup

--- recipeBok/src/app/shopping-list/shopping-list.component.ts
+++ recipeBok/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { from } from 'rxjs';
+import { Ingredient } from "../shared/ingredient.model";
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients: Ingredient[] = [
+    new Ingredient("mango", 10),
+    new Ingredient("Apple", 45),
+    new Ingredient("mango", 10),
+    new Ingredient("Apple", 45),
+    new Ingredient("mango", 10),
+    new Ingredient("Apple", 45),
+  ];
   constructor() { }
 
   ngOnInit(): void {
